79871513933ac6b6e40488f126a5b0a9d5070e6e
Muhammad Alfauzan <muhammadalfauan254@gmail.com>
2025-01-16 07:45:31 +0700

    done feature login, set up ksp hilt

---
+++ app/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("kotlin-parcelize")
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.finalproject.technicaltest"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.finalproject.technicaltest"
+        minSdk = 27
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+}
---
+++ app/src/main/java/com/finalproject/technicaltest/ui/LoginFragment.kt
@@ -0,0 +1,134 @@
+package com.finalproject.technicaltest.ui
+
+import android.graphics.drawable.GradientDrawable
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.finalproject.technicaltest.R
+import com.finalproject.technicaltest.databinding.FragmentLoginBinding
+import com.google.android.material.textfield.TextInputLayout
+
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        setupTextWatchers()
+        binding.includeCustomButtonPass.btnLogin.setOnClickListener { showProgressBar() }
+        toggleLoginButtonState()
+
+        return binding.root
+    }
+
+    private fun setupTextWatchers() {
+        binding.edEmail.addTextChangedListener(createTextWatcher(binding.textInputLayout))
+        binding.edPassword.addTextChangedListener(createTextWatcher(binding.textInputLayout2))
+    }
+
+    private fun createTextWatcher(inputLayout: TextInputLayout): TextWatcher {
+        return object : TextWatcher {
+            override fun beforeTextChanged(
+                charSequence: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int,
+            ) {
+            }
+
+            override fun onTextChanged(
+                charSequence: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int,
+            ) {
+                handleInputValidation(charSequence, inputLayout)
+            }
+
+            override fun afterTextChanged(editable: Editable?) {}
+        }
+    }
+
+    private fun handleInputValidation(charSequence: CharSequence?, inputLayout: TextInputLayout) {
+        inputLayout.error = if (charSequence.isNullOrEmpty()) "Field cannot be empty" else null
+        toggleLoginButtonState()
+    }
+
+    private fun performLogin() {
+        val username = binding.edEmail.text.toString().trim()
+        val password = binding.edPassword.text.toString().trim()
+
+        when {
+            username.isEmpty() || password.isEmpty() -> {
+                Toast.makeText(
+                    requireContext(),
+                    "Username and Password cannot be empty",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            username == "alfagift-admin" && password == "asdf" -> {
+                Toast.makeText(requireContext(), "Login Successful", Toast.LENGTH_SHORT).show()
+            }
+
+            else -> {
+                Toast.makeText(requireContext(), "Wrong Username or Password", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
+    private fun toggleLoginButtonState() {
+        val isUsernameFilled = binding.edEmail.text?.isNotEmpty() == true
+        val isPasswordFilled = binding.edPassword.text?.isNotEmpty() == true
+        val isButtonEnabled = isUsernameFilled && isPasswordFilled
+        binding.includeCustomButtonPass.btnLogin.isEnabled = isButtonEnabled
+
+
+        val buttonColor = if (isButtonEnabled) {
+            ContextCompat.getColor(requireContext(), R.color.blue)
+        } else {
+            ContextCompat.getColor(requireContext(), R.color.gray)
+        }
+
+        val drawable = ContextCompat.getDrawable(requireContext(), R.drawable.bg_button) as GradientDrawable
+        drawable.setColor(buttonColor) // Set warna tombol berdasarkan kondisi
+        binding.includeCustomButtonPass.btnLogin.background = drawable
+
+        val buttonText = if (isButtonEnabled) {
+            getString(R.string.button_masuk)
+        } else {
+            getString(R.string.button_masuk)
+        }
+
+        binding.includeCustomButtonPass.tvLoading.text = buttonText
+    }
+
+    private fun showProgressBar() {
+        binding.includeCustomButtonPass.progressBar.visibility = View.VISIBLE
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            performLogin()
+            binding.includeCustomButtonPass.progressBar.visibility = View.GONE
+        }, 3000)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
